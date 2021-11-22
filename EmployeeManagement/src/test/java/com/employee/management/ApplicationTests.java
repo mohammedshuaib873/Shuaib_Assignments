ce145e40110867beee66293d235845a6e88c8c0d
mtippusu <mtippusu@LIN80032184.corp.capgemini.com>
2021-11-22 17:33:59 +0530

    Employee Management System Code Committed

---
+++ EmployeeManagement/src/test/java/com/employee/management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ HelloWorldAssignment/src/test/java/HelloWorldAssignment/HelloWorldAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package HelloWorldAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
