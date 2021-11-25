0e6b31f752410cb8e323e9e72c8b3beda49f21b0
mtippusu <mtippusu@LIN80032184.corp.capgemini.com>
2021-11-25 23:41:53 +0530

    Code Committed Successfully

---
+++ SpringMVCAssignment2/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>SI Calculation</title>
+</head>
+<body>
+<form action="calculate">
+  <label for="pa">Principal Amount:</label><br>
+  <input type="text" id="pa" name="inp1"><br>
+  <label for="ny">Number Of Years:</label><br>
+  <input type="text" id="ny" name="inp2"><br>
+  <label for="ri">Rate Of Interest:</label><br>
+  <input type="text" id="ri" name="inp3"><br>
+  <input type="submit">
+</form>
+</body>
+</html>
---
+++ SpringMVCAssignment3/src/main/java/com/SpringMVCAssignment3/User.java
@@ -0,0 +1,32 @@
+package com.SpringMVCAssignment3;
+
+public class User {
+	
+	private String emailId;
+	private String userName;
+	private String password;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [emailId=" + emailId + ", userName=" + userName + ", password=" + password + "]";
+	}
+	
+	
+}
