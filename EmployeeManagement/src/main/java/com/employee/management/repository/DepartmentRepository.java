2f8ca5416d3df7ba6fc83c3f94124e0404a348e4
mohammedshuaib873 <47596857+mohammedshuaib873@users.noreply.github.com>
2021-11-29 01:40:56 +0530

    Delete EmployeeManagement directory

--- EmployeeManagement/src/main/java/com/employee/management/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.employee.management.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.employee.management.model.Department;
-
-public interface DepartmentRepository extends CrudRepository<Department, Integer>{
-
-}
